5ebaa6fd3d057f425f629d3da9755923f3c8eb9b
Koray Tugay <koraytugay@icloud.com>
2026-05-02 09:25:15 -0400

    Update data.js

--- data.js
+++ data.js
@@ -32,4 +32,5 @@ const gameData = [
   { date: "2026-04-30 14:30", extensions: [3], won: false },
   { date: "2026-04-30 18:30", extensions: [3], won: false },
   { date: "2026-05-01 09:30", extensions: [3], won: false },
+  { date: "2026-05-02 08:30", extensions: [3], won: true },
 ];
